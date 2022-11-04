fab0a91d180baeaeabd47f6c0a9620dc9546da6a
estebanjy <yjuanesteban@gmail.com>
2022-11-04 13:47:22 -0300

    Mod ModuloDeteccionLinea

--- ModuloDeteccionLinea.py
+++ ModuloDeteccionLinea.py
@@ -1,32 +1,82 @@
 import cv2
-
-
-camara = cv2.VideoCapture(0)
-while True:
-    _, frame = camara.read()
-
-    Blanco = cv2.inRange(frame, (200,200,200) , (250, 250, 250))
-
-    contorno, _ = cv2.findContours(Blanco.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    for c in contorno:
-        area = cv2.contourArea(c)
-        if area > 3000:
-            if len(contorno) != 0:
-                d = max(contorno, key=cv2.contourArea)
-
-            M = cv2.moments(d)
-            if (M["m00"] == 0):
-                M["m00"] = 1
-            x = int(M["m10"] / M["m00"])
-            y = int(M["m01"] / M["m00"])
-            cv2.circle(frame, (x, y), 20, (0, 0, 255), 2)
-            nuevoContorno = cv2.convexHull(d)
-
-            cv2.drawContours(frame, [nuevoContorno], -1, (255, 0, 0), 3)
-
-    cv2.imshow("original", frame)
-    cv2.imshow("BINARIA", Blanco)
-
-    if cv2.waitKey(1) & 0xFF == ord("f"):
-        break
+import numpy as np
+import utlis
+ 
+curveList = []
+avgVal=10
+ 
+def getLaneCurve(img,display=2):
+ 
+    imgCopy = img.copy()
+    imgResult = img.copy()
+    #### STEP 1
+    imgThres = utlis.thresholding(img)
+ 
+    #### STEP 2
+    hT, wT, c = img.shape
+    points = utlis.valTrackbars()
+    imgWarp = utlis.warpImg(imgThres,points,wT,hT)
+    imgWarpPoints = utlis.drawPoints(imgCopy,points)
+ 
+    #### STEP 3
+    middlePoint,imgHist = utlis.getHistogram(imgWarp,display=True,minPer=0.5,region=4)
+    curveAveragePoint, imgHist = utlis.getHistogram(imgWarp, display=True, minPer=0.9)
+    curveRaw = curveAveragePoint - middlePoint
+ 
+    #### SETP 4
+    curveList.append(curveRaw)
+    if len(curveList)>avgVal:
+        curveList.pop(0)
+    curve = int(sum(curveList)/len(curveList))
+ 
+    #### STEP 5
+    if display != 0:
+        imgInvWarp = utlis.warpImg(imgWarp, points, wT, hT, inv=True)
+        imgInvWarp = cv2.cvtColor(imgInvWarp, cv2.COLOR_GRAY2BGR)
+        imgInvWarp[0:hT // 3, 0:wT] = 0, 0, 0
+        imgLaneColor = np.zeros_like(img)
+        imgLaneColor[:] = 0, 255, 0
+        imgLaneColor = cv2.bitwise_and(imgInvWarp, imgLaneColor)
+        imgResult = cv2.addWeighted(imgResult, 1, imgLaneColor, 1, 0)
+        midY = 450
+        cv2.putText(imgResult, str(curve), (wT // 2 - 80, 85), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 255), 3)
+        cv2.line(imgResult, (wT // 2, midY), (wT // 2 + (curve * 3), midY), (255, 0, 255), 5)
+        cv2.line(imgResult, ((wT // 2 + (curve * 3)), midY - 25), (wT // 2 + (curve * 3), midY + 25), (0, 255, 0), 5)
+        for x in range(-30, 30):
+            w = wT // 20
+            cv2.line(imgResult, (w * x + int(curve // 50), midY - 10),
+                     (w * x + int(curve // 50), midY + 10), (0, 0, 255), 2)
+        #fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+        #cv2.putText(imgResult, 'FPS ' + str(int(fps)), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (230, 50, 50), 3);
+    if display == 2:
+        imgStacked = utlis.stackImages(0.7, ([img, imgWarpPoints, imgWarp],
+                                             [imgHist, imgLaneColor, imgResult]))
+        cv2.imshow('ImageStack', imgStacked)
+    elif display == 1:
+        cv2.imshow('Resutlt', imgResult)
+ 
+    #### NORMALIZATION
+    curve = curve/100
+    if curve>1: curve ==1
+    if curve<-1:curve == -1
+ 
+    return curve
+ 
+ 
+if __name__ == '__main__':
+    cap = cv2.VideoCapture('vid1.mp4')
+    intialTrackBarVals = [102, 80, 20, 214 ]
+    utlis.initializeTrackbars(intialTrackBarVals)
+    frameCounter = 0
+    while True:
+        frameCounter += 1
+        if cap.get(cv2.CAP_PROP_FRAME_COUNT) == frameCounter:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            frameCounter = 0
+ 
+        success, img = cap.read()
+        img = cv2.resize(img,(480,240))
+        curve = getLaneCurve(img,display=2)
+        print(curve)
+        #cv2.imshow('Vid',img)
+        cv2.waitKey(1)
